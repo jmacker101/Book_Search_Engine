6522297de4581b227c7e6ae23f57be0973a262aa
jmacker101 <jm470t@gmail.com>
2024-06-15 03:39:52 -0400

    resolvers.js fixing deletebook

--- Assets/server/schemas/resolvers.js
+++ Assets/server/schemas/resolvers.js
@@ -1,54 +1,89 @@
-const { Book, User } = require('../models');
-const { signToken, AuthError } = require('../utils/auth')
+// Importing the User model and authentication utilities
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+// GraphQL resolvers defining the Query and Mutation operations
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => { 
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id}).select('-__v -password');
-        return userData 
+    // Resolver to get the current logged-in user and their saved books
+    me: async (parent, args, context) => {
+      const foundUser = await User.findOne({
+        _id: context.user._id,
+      }).populate("savedBooks");
+
+      if (!foundUser) {
+        // Throw AuthenticationError if user is not found
+        throw AuthenticationError;
       }
-      throw AuthError
-    }
+
+      return foundUser;
+    },
   },
   Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args)
-      const token = signToken(user)
-      return {user, token}
-    },
-    logIn: async (parent, { email, password }) => {
-      const user = await User.findOne({ email })
+    // Resolver to create a new user, sign their info to JWT token, and return both the token and user
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({
+        username: username,
+        email: email,
+        password: password,
+      });
+
       if (!user) {
-        throw AuthError
+        // Throw AuthenticationError if user creation fails
+        throw AuthenticationError;
       }
-      const correctPassword = await user.isCorrectPassword(password)
-      if (!correctPassword) {
-        throw AuthError
-      }
-      const token = signToken(user)
-      return {user, token}
+      const token = signToken(user);
+      return { token, user };
     },
-    saveBook: async (parent, {bookData}, context) => {
-      if (context.user) {
-        const updateUser = await User.findByIdAndUpdate({ _id: context.user._id },
-          { $push: { savedBooks: bookData } },
-          { new: true })
-        return updateUser
+    // Resolver to login a user, sign their token for authentication, and return the token and user object
+    login: async (parent, { email, password }) => {
+      // Find a user by email in the database and verify their password
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        // Throw AuthenticationError if user is not found
+        throw AuthenticationError;
       }
-      throw AuthError
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        // Throw AuthenticationError if password is incorrect
+        throw AuthenticationError;
+      }
+      const token = signToken(user);
+      return { token, user };
     },
-    removeBook: async (parent, {bookId}, context) => {
-      if (context.user) {
-        const updateUser = await User.findByIdAndUpdate({ _id: context.user._id },
-          { $pull: { savedBooks: {bookId} } },
-          { new: true })
-        return updateUser
+    // Resolver to save a book to a user's `savedBooks` subdocument
+    saveBook: async (parent, { content }, context) => {
+      try {
+        // Find the user by ID and update their `savedBooks` field
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: content } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+        // Throw AuthenticationError if an error occurs
+        throw AuthenticationError;
       }
-      throw AuthError
-
-    }
-
-  }
+    },
+    // Resolver to delete a book by ID from the user's `savedBooks` subdocument
+    deleteBook: async (parent, params, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        // Throw AuthenticationError if user is not found
+        throw AuthenticationError;
+      }
+      return updatedUser;
+    },
+  },
 };
 
+// Exporting the resolvers for use in Apollo Server
 module.exports = resolvers;
